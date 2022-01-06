85dd64045e149d13bf24ad673056838fc88a880b
jjo <k01066624566@gmail.com>
2022-01-06 09:49:30 +0900

    fix memberServiceTest(실행순서가 보장되지 않아서 첫번째 테스트가 깨지던 경우)

--- src/test/java/com/example/delivery/service/MemberServiceTest.java
+++ src/test/java/com/example/delivery/service/MemberServiceTest.java
@@ -3,14 +3,14 @@ package com.example.delivery.service;
 import com.example.delivery.domain.Member;
 import com.example.delivery.dto.MemberDto;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MemberServiceTest {
     @Autowired
     private MemberService memberService;
@@ -21,12 +21,14 @@ class MemberServiceTest {
     }
 
     @Test
+    @Order(1)
     public void join(){
         MemberDto memberDto = new MemberDto("email@gmail.com", "test", "1234");
         Long join = memberService.join(memberDto);
         Assertions.assertThat(join).isEqualTo(1L);
     }
     @Test
+    @Order(2)
     public void findOne(){
         //given
         MemberDto memberDto = new MemberDto("email@gmail.com", "test", "1234");
