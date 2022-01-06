2d87bb2e06efe1ce3c1209314bb3c267972db171
jjo <k01066624566@gmail.com>
2022-01-06 19:05:43 +0900

    fix memberService

--- src/main/java/com/example/delivery/service/MemberService.java
+++ src/main/java/com/example/delivery/service/MemberService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,6 +24,9 @@ public class MemberService {
         Optional<Member> byId = memberRepository.findById(id);
         Member member = byId.get();
         return member;
+    }
+    public List<Member> findAll(){
+        return memberRepository.findAll();
     }
     public void deleteOne(Long memberIdx){
         memberRepository.deleteById(memberIdx);
